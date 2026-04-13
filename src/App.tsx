7d031d1edf348b367f76c4e43e0cfd076929a9cb
Ruma Choudhury <rumachoudhury7862@gmail.com>
2026-04-13 16:31:46 -0400

    Updated some part

--- src/App.tsx
+++ src/App.tsx
@@ -1,118 +1,3 @@
-// import { useState } from "react";
-// // import reactLogo from "./assets/react.svg";
-// // import viteLogo from "./assets/vite.svg";
-// // import heroImg from "./assets/hero.png";
-// import "./App.css";
-
-// import { supabase } from "./supabase-client";
-
-// function App() {
-//   const [newtask, setNewTask] = useState({
-//     title: "",
-//     description: "",
-//   });
-
-//   const handleSubmit = async (e: any) => {
-//     e.preventDefault();
-//     const { error } = await supabase.from("tasks").insert([newtask]);
-
-//     if (error) {
-//       console.error("Error adding task:", error.message);
-//     }
-
-//     setNewTask({ title: "", description: "" });
-//   };
-//   return (
-//     <div className="body">
-//       <div
-//         style={{
-//           maxWidth: "600px",
-//           margin: "0 auto",
-//           background: "#f9f9f9",
-//           padding: "2rem",
-//           borderRadius: "8px",
-//           marginTop: "2rem",
-//           boxShadow: "0 4px 12px rgba(0,0,0,0.2)",
-//         }}
-//       >
-//         <h2 style={{ color: "#e05915ff", textAlign: "center" }}>
-//           Task Manager CRUD
-//         </h2>
-
-//         <form
-//           onSubmit={handleSubmit}
-//           style={{
-//             marginBottom: "1rem",
-//             border: "1px solid #ccc",
-//             padding: "1rem",
-//             borderRadius: "4px",
-//           }}
-//         >
-//           <input
-//             type="text"
-//             name=""
-//             id=""
-//             placeholder="Task Title"
-//             onChange={(e) =>
-//               setNewTask((prev) => ({ ...prev, title: e.target.value }))
-//             }
-//             style={{
-//               width: "100%",
-//               marginBottom: "0.5rem",
-//               padding: "0.5rem",
-//               border: "1px solid #ccc",
-//               borderRadius: "4px",
-//             }}
-//           />
-//           <textarea
-//             name=""
-//             id=""
-//             placeholder="Task Description"
-//             onChange={(e) =>
-//               setNewTask((prev) => ({ ...prev, description: e.target.value }))
-//             }
-//             style={{
-//               width: "100%",
-//               marginBottom: "0.5rem",
-//               padding: "0.5rem",
-//             }}
-//           ></textarea>
-//           <button
-//             type="submit"
-//             style={{
-//               padding: "0.5rem 1rem",
-//               color: "#fff",
-//               backgroundColor: "#e05915ff",
-//               border: "none",
-//               borderRadius: "4px",
-//             }}
-//           >
-//             Add Task
-//           </button>
-//         </form>
-
-//         {/* Task List */}
-//         <ul style={{ listStyle: "none", padding: "0" }}>
-//           <li
-//             style={{
-//               border: "1px solid #ccc",
-//               padding: "1rem",
-//               marginBottom: "0.5rem",
-//             }}
-//           >
-//             {/* <strong>{task.title}</strong> */}
-//             {/* <p>{task.description}</p> */}
-//             No tasks yet
-//           </li>
-//         </ul>
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default App;
-
-// =============================================================================
 import { useState } from "react";
 import "./App.css";
 import { supabase } from "./supabase-client";
@@ -125,10 +10,24 @@ function App() {
 
   const [darkMode, setDarkMode] = useState(false);
 
-  const handleSubmit = async (e: React.FormEvent) => {
+  const fetchTasks = async () => {
+    const { data, error } = await supabase
+      .from("tasks")
+      .select("*")
+      .order("created_at", { ascending: true });
+
+    if (error) {
+      console.log("Error:", error.message);
+      return;
+    }
+
+    console.log(data);
+  };
+
+  const handleSubmit = async (e: any) => {
     e.preventDefault();
 
-    const { error } = await supabase.from("tasks").insert([newtask]);
+    const { error } = await supabase.from("tasks").insert(newtask).single();
 
     if (error) {
       console.error("Error:", error.message);
