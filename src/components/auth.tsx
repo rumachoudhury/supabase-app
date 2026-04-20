47c9d1ca741ad6565eed406f021cc5117000f2f0
Ruma Choudhury <rumachoudhury7862@gmail.com>
2026-04-20 15:11:16 -0400

    Updated auth.js file

--- src/components/auth.tsx
+++ src/components/auth.tsx
@@ -1,68 +1,3 @@
-// import { useState, type ChangeEvent } from "react";
-// import { supabase } from "../supabase-client";
-
-// export const Auth = () => {
-//   const [isSignUp, setIsSignUp] = useState(false);
-//   const [email, setEmail] = useState("");
-//   const [password, setPassword] = useState("");
-
-//   const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
-//     e.preventDefault();
-
-//     if (isSignUp) {
-//       const { error: signUpError } = await supabase.auth.signUp({
-//         email,
-//         password,
-//       });
-
-//       if (signUpError) {
-//         console.error("Error signing up", signUpError.message);
-//         return;
-//       }
-//     } else {
-//       const { error: signInError } = await supabase.auth.signInWithPassword({
-//         email,
-//         password,
-//       });
-
-//       if (signInError) {
-//         console.error("Error signing in:", signInError.message);
-//         return;
-//       }
-//     }
-//   };
-
-//   return (
-//     <div style={{ maxWidth: "400px", margin: "0 auto", padding: "1rem" }}>
-//       <h2>{isSignUp ? "Sign Up" : "Sign In"}</h2>
-
-//       <form onSubmit={handleSubmit}>
-//         <input
-//           type="email"
-//           placeholder="Email"
-//           value={email}
-//           onChange={(e: ChangeEvent<HTMLInputElement>) =>
-//             setEmail(e.target.value)
-//           }
-//         />
-
-//         <input
-//           type="password"
-//           placeholder="Password"
-//           value={password}
-//           onChange={(e: ChangeEvent<HTMLInputElement>) =>
-//             setPassword(e.target.value)
-//           }
-//           style={{ width: "100%", marginBottom: "0.5rem", padding: "0.5rem" }}
-//         />
-
-//         <button type="submit">{isSignUp ? "Sign Up" : "Sign In"}</button>
-//       </form>
-//     </div>
-//   );
-// };
-
-// ----------------------
 import { useState, type ChangeEvent, type FormEvent } from "react";
 import { supabase } from "../supabase-client";
 import "../style/Auth.css";
@@ -85,7 +20,7 @@ export const Auth = () => {
 
     // simple validation
     if (!email.includes("@")) {
-      setErrorMsg("Please enter a valid email.");
+      setErrorMsg("Please enter your valid email.");
       setLoading(false);
       return;
     }
